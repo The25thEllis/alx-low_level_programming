466bf7b7b1a733e07d0c495fbd10ddf9b5f62ced
the25thellis <ologunellis273@gmail.com>
2022-09-22 22:46:50 +0100

    all questions answered

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	system("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
